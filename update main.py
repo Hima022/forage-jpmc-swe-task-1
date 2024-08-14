6ee866e8c826135b8ae3a8d39f099d855713f996
Hima022 <114853806+Hima022@users.noreply.github.com>
2024-08-14 16:57:36 +0530

    Update and rename update client3.py to update main.py

--- update main.py
+++ update main.py
@@ -18,16 +18,6 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
-import json
-import random
-import urllib.request
-
-# Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
-
-# 500 server request
-N = 500
-
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
@@ -39,26 +29,3 @@ def getDataPoint(quote):
     return stock, bid_price, ask_price, price
 
 
-def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    if(price_b := 0):
-        return
-    return price_a/price_b
-    
-
-
-# Main
-if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
-        price={}
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock]=price
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(prices[ABC], prices[DEF]))
